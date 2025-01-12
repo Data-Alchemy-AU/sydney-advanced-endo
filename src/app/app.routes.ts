06da7015fa20f895bfee70d99d4842d27fb3f5bd
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-01-13 09:18:23 +1100

    fix: home path to /home and redirect ''

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,11 @@ import {ContactUsComponent} from "./contact-us/contact-us.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'doctors', component: DoctorsComponent },
   { path: 'contact-us', component: ContactUsComponent},
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import { RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+    imports: [
+        NgOptimizedImage, RouterOutlet, RouterLink, RouterLinkActive
+    ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
