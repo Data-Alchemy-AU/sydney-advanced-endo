d789cfbf6efa31ceb4d0b6754ab9b5b65c538879
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-01-14 09:49:38 +1100

    fix/navbar colors

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,13 +4,15 @@ import {NgOptimizedImage} from "@angular/common";
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {LogoComponent} from "../logo/logo.component";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     MatButtonModule, MatIconModule,
-    RouterOutlet, RouterLink, RouterLinkActive, NgOptimizedImage, LogoComponent
+    RouterOutlet, RouterLink, RouterLinkActive, NgOptimizedImage, LogoComponent, MatToolbarModule, MatMenuModule
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
