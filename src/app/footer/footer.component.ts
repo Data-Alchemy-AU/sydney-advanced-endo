455baf8968ffe34dd7e155de99364b8fcf2dd96a
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-01-15 09:05:01 +1100

    fix/footer copy text

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
 import { RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 import {LogoComponent} from "../logo/logo.component";
 import {MatAnchor, MatButton} from "@angular/material/button";
 import {MatToolbar} from "@angular/material/toolbar";
-import {MatSidenavContainer} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    NgOptimizedImage, RouterOutlet, RouterLink, RouterLinkActive, LogoComponent, MatAnchor, MatButton, MatToolbar, MatSidenavContainer
+    RouterOutlet, RouterLink, RouterLinkActive, LogoComponent, MatAnchor, MatButton, MatToolbar
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
