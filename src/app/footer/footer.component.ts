692df7c5ecc2a9e52a5dec65319c4f48491e5d7a
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-01-15 08:32:40 +1100

    chore/removed bootstrap css

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,16 +4,16 @@ import { RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 import {LogoComponent} from "../logo/logo.component";
 import {MatAnchor, MatButton} from "@angular/material/button";
 import {MatToolbar} from "@angular/material/toolbar";
+import {MatSidenavContainer} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    NgOptimizedImage, RouterOutlet, RouterLink, RouterLinkActive, LogoComponent, MatAnchor, MatButton, MatToolbar
+    NgOptimizedImage, RouterOutlet, RouterLink, RouterLinkActive, LogoComponent, MatAnchor, MatButton, MatToolbar, MatSidenavContainer
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
 }
