0d13bb4a1fa308aaee2fcb0ad882b8e33025179a
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-01-15 07:57:13 +1100

    fix/display for header and footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import { RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 import {LogoComponent} from "../logo/logo.component";
+import {MatAnchor, MatButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    NgOptimizedImage, RouterOutlet, RouterLink, RouterLinkActive, LogoComponent
+    NgOptimizedImage, RouterOutlet, RouterLink, RouterLinkActive, LogoComponent, MatAnchor, MatButton, MatToolbar
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
