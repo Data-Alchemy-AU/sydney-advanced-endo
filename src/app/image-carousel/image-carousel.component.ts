097d5678dd53eeb72c09a00c11f34a7e9c402346
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-01-15 10:23:53 +1100

    add/carousel placeholder

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-image-carousel',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './image-carousel.component.html',
   styleUrl: './image-carousel.component.scss'
 })
 export class ImageCarouselComponent {
-
+  imageUrl = './assets/Carousel/image1.jpeg';
+  imageAlt = 'Banner image';
 }
