570b0410e46aba5a8dc2ea66bee866d141373cec
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-02-08 14:59:40 +1100

    fix/some spelling

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {MapComponent} from "../map/map.component";
 import {ButtonSharedComponent} from "../components/buttons/button-shared/button-shared.component";
+import {MatDivider} from "@angular/material/divider";
+import {OurTeamSummaryComponent} from "../our-team-summary/our-team-summary.component";
 @Component({
     selector: 'app-contact-us',
-  imports: [
-    MapComponent,
-    ButtonSharedComponent
-  ],
+    imports: [
+        MapComponent,
+        ButtonSharedComponent,
+        MatDivider,
+        OurTeamSummaryComponent
+    ],
     templateUrl: './contact-us.component.html',
     styleUrl: './contact-us.component.scss'
 })
