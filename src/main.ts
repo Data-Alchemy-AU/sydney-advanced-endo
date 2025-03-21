6c59aac0375ca52499322368a17313c715bb4a30
leahshepherd <lshe9740@uni.sydney.edu.au>
2025-03-21 18:18:50 +1100

    update to routes for compatibility with ghpages

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import {AppComponent} from './app/app.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error("error:#%d",err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error("error:#%d",err));
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }, // ✅ THIS WORKS
+  ],
+});
 
